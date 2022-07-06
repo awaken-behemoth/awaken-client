418887e0102f5857a6c12bf545e6b0b8a749d9f2
Behemoth11 <kodohdaniel@gmail.com>
2022-07-05 20:35:11 -0500

    finished setting up react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,33 @@
 import "../styles/globals.css";
-import Wrapper from "../components/Layout/PageWrapper";
 import { NextPage } from "next";
-import { JSXElementConstructor, ReactElement, ReactNode } from "react";
 import { AppProps } from "next/app";
+import Wrapper from "../components/Layout/PageWrapper";
+import { JSXElementConstructor, ReactElement, useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
+/** Next js page that uses dynamic layout */
 type NextPageWithLayout = NextPage & {
   getLayout?: (
     page: ReactElement<any, JSXElementConstructor<any>>
-  ) =>  ReactElement<any, JSXElementConstructor<any>>;
+  ) => ReactElement<any, JSXElementConstructor<any>>;
 };
 
+/** Declares component that represents the page currently being rendered */
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
 const MyApp: React.FC<AppPropsWithLayout> = ({ Component, pageProps }) => {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const [queryClient] = useState(() => new QueryClient());
 
-  return <Wrapper child={getLayout(<Component {...pageProps} />)}></Wrapper>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Wrapper child={getLayout(<Component {...pageProps} />)} ></Wrapper>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
